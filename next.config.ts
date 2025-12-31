1a751ed439369b183a11e50099e2ff1057bbe347
Mark Joshua Austria <mark.a@wphdigital.com>
2025-12-31 12:39:46 +0800

    chore: update next-pwa dependency and remove outdated types

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
-import withPWA from "next-pwa";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {};
 
 export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
   ...nextConfig,
 });
